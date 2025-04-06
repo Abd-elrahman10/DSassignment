65be97adec4e867c44c17630d9dd042fa713bb1c
Medhat Orabi <162382370+MedhatElhawy@users.noreply.github.com>
2025-04-06 22:45:26 +0200

    Problem2.cpp

--- Problem2.cpp
+++ Problem2.cpp
@@ -22,9 +22,20 @@ public:
         delete[] coeffsPolynomial;
     }
 
-void Display() {
-        bool firstTerm = true;
+    bool isInvalid() const {
+        bool allZeroLHS = true;
+        for (int i = OrderOfPolynomial + 1; i > 0; --i) {
+            if (coeffsPolynomial[i] != 0) {
+                allZeroLHS = false;
+                break;
+            }
+        }
+        int rhs = coeffsPolynomial[0];
+        return allZeroLHS && (rhs != 0);
+    }
 
+    void Display() {
+        bool firstTerm = true;
         for (int i = OrderOfPolynomial + 1; i >= 0; i--) {
             int coeff = coeffsPolynomial[i];
             if (coeff == 0 && i != 0) {
@@ -33,9 +44,9 @@ void Display() {
             if (!firstTerm && i != 0) {
                 (coeff > 0)? cout << " + " : cout << " - ";
             }
-            else if (coeff < 0)
-                cout << "- ";
-            if (i == 0) {
+            else if (coeff < 0 && coeff != 0)
+                cout << " - ";
+            if (i == 0 ) {
                 cout << " = " << coeff;
             }
             else if (i == 1) cout << abs(coeff);
@@ -92,11 +103,27 @@ void Display() {
 
 };
 
+
 void FillPolynomial(int OrderOfPolynomial, int coeffsPolynomial[]) {
-        for (int i = 0; i <= OrderOfPolynomial + 1; i++) {
-            cin >> coeffsPolynomial[i];
+    for (int i = 0; i <= OrderOfPolynomial + 1; i++) {
+        while (!(cin >> coeffsPolynomial[i])) {
+            cout << "Invalid input. Please enter a coeff: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
-        cout << endl;
+    }
+    cout << endl;
+}
+
+int getOrder()
+{
+    int order;
+    while (!(cin >> order)) {
+        cout << "Invalid input. Please enter an Order: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return order;
 }
 
 int main() {
@@ -104,7 +131,7 @@ int main() {
     // input by file
     ifstream inputFile("Problem2_testcases.txt");
     if (!inputFile) {
-        cout << "Error: Cannot open test_cases.txt" << endl;
+        cout << "Error: Cannot open Problem2_testcases.txt" << endl;
         return 0;
     }
 
@@ -125,14 +152,36 @@ int main() {
         Polynomial P2(Secondorder, coeffsSecondpolynomial);
 
         cout << "First Polynomial: ";
-        P1.Display();
+        if (P1.isInvalid()) {
+            cout << "Error: Invalid polynomial (left-hand side is zero but right-hand side is not zero)." << endl;
+        } else {
+            P1.Display();
+        }
+
         cout << "Second Polynomial: ";
-        P2.Display();
+        if (P2.isInvalid()) {
+            cout << "Error: Invalid polynomial (left-hand side is zero but right-hand side is not zero)." << endl;
+        } else {
+            P2.Display();
+        }
+
+        Polynomial sum = P1 + P2;
+        Polynomial diff = P2 - P1;
 
         cout << "Sum of polynomials: ";
-        (P1 + P2).Display();
+        if (sum.isInvalid()) {
+            cout << "Error: Invalid polynomial (left-hand side is zero but right-hand side is not zero)." << endl;
+        } else {
+            sum.Display();
+        }
+
         cout << "Difference of polynomials: ";
-        (P2 - P1).Display();
+        if (diff.isInvalid()) {
+            cout << "Error: Invalid polynomial (left-hand side is zero but right-hand side is not zero)." << endl;
+        } else {
+            diff.Display();
+        }
+
         cout << "----------------------------" << endl;
 
         delete[] coeffsFirstpolynomial;
@@ -141,64 +190,93 @@ int main() {
 
     inputFile.close();
 
-
     // input by user
 
     /*int Firstorder = 0, Secondorder = 0;
     cout << "Order of first polynomial: ";
-    cin >> Firstorder;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    Firstorder = getOrder();
     int* coeffsFirstpolynomial = new int[Firstorder + 1];
     cout << "Enter coefficients for the first polynomial: ";
     FillPolynomial(Firstorder, coeffsFirstpolynomial);
 
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cout << "Order of second polynomial: ";
-    cin >> Secondorder;
+    Secondorder = getOrder();
     int* coeffsSecondpolynomial = new int[Secondorder + 1];
     cout << "Enter coefficients for the second polynomial: ";
     FillPolynomial(Secondorder, coeffsSecondpolynomial);
+
     Polynomial P1(Firstorder, coeffsFirstpolynomial);
     Polynomial P2(Secondorder, coeffsSecondpolynomial);
-    cout << "First Polynomial: ";
-    P1.Display();
-    cout << "Second Polynomial: ";
-    P2.Display();
-    cout << "Sum of polynomials: ";
-    (P1 + P2).Display();
-    cout << "Difference of polynomials: ";
-    (P2 - P1).Display();
 
+    cout << "First polynomial: ";
+    if (P1.isInvalid())
+    {
+        cout << "Error: Invalid polynomial (left-hand side is zero but right-hand side is not zero)." << endl;
+
+    }
+    else{
+        P1.Display();
+    }
+
+    cout << "Second polynomial: ";
+     if (P2.isInvalid())
+    {
+        cout << "Error: Invalid polynomial (left-hand side is zero but right-hand side is not zero)." << endl;
+
+    }
+    else{
+        P2.Display();
+    }
+
+    Polynomial sum(P1 + P2);
+    Polynomial diff(P2 - P1);
+
+    cout << "Sum of polynomials: ";
+    if (sum.isInvalid()) {
+        cout << "Error: Invalid polynomial (left-hand side is zero but right-hand side is not zero)." << endl;
+    }
+    else {
+        sum.Display();
+    }
+
+    cout << "Difference of polynomials: ";
+    if (diff.isInvalid()) {
+        cout << "Error: Invalid polynomial (left-hand side is zero but right-hand side is not zero)." << endl;
+    }
+    else {
+        diff.Display();
+    }
     delete[] coeffsFirstpolynomial;
     delete[] coeffsSecondpolynomial;
     */
+
     return 0;
 }
 
 /*
 test1:
-Input: 
-Order of first polynomial: 2   
-Enter polynomial: 0 1 3 2   
-Order of second polynomial: 4   
-Enter polynomial: 8 0 4 0 0 3   
-Output: 
-First polynomial: 2x^2 + 3x + 1 = 0   
-Second polynomial: 3x^4 + 4x = 8   
-Sum of polynomials: 3x^4 + 2x^2 + 7x + 1 = 8   
+Input:
+Order of first polynomial: 2
+Enter polynomial: 0 1 3 2
+Order of second polynomial: 4
+Enter polynomial: 8 0 4 0 0 3
+Output:
+First polynomial: 2x^2 + 3x + 1 = 0
+Second polynomial: 3x^4 + 4x = 8
+Sum of polynomials: 3x^4 + 2x^2 + 7x + 1 = 8
 Difference of polynomials: 3x^4 - 2x^2 + x - 1 = 8
 
 
 
 test2:
-Input: 
-Order of first polynomial: 3   
-Enter polynomial: 0 5 0 1 4   
-Order of second polynomial: 3   
-Enter polynomial: 0 2 3 0 6   
-Output: 
-First polynomial: 4x^3 + x^2 + 5 = 0   
-Second polynomial: 6x^3 + 3x + 2 = 0   
-Sum of polynomials: 10x^3 + x^2 + 3x + 7 = 0   
+Input:
+Order of first polynomial: 3
+Enter polynomial: 0 5 0 1 4
+Order of second polynomial: 3
+Enter polynomial: 0 2 3 0 6
+Output:
+First polynomial: 4x^3 + x^2 + 5 = 0
+Second polynomial: 6x^3 + 3x + 2 = 0
+Sum of polynomials: 10x^3 + x^2 + 3x + 7 = 0
 Difference of polynomials: 2x^3 - x^2 + 3x - 3 = 0
 */
